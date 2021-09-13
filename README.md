8bf91d7c22bedc6a2db43b93f8ab6390980976fb
Phanuwat Phalad <phanuwat.davidcopter@gmail.com>
2021-09-13 10:50:57 +0700

    Initial setup webserver and database
    Setup Go Echo and Gorm for connecting to MySQL database

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# golang-mysql-jwt-crash-course-101
+# Gobook Crash Course 101
  golang crash couse project: `Gobook` like facebook  with user-service and post-service using mysql and jwt
+
+# Setup
+
+- `Echo` setup
+
+```bash
+mkdir myapp && cd myapp
+go mod init myapp
+go get github.com/labstack/echo/v4
+```
+
+- `Gorm` setup
+
+```bash
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/mysql
+```
+
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	dsn := "root:example@tcp(127.0.0.1:3306)/gobook?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
