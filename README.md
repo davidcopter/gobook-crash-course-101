f31bc5ffc9e9d83ad9c766d8c0cc5a02b0043c2f
Phanuwat Phalad <phanuwat.davidcopter@gmail.com>
2021-09-14 21:22:29 +0700

    feat: add jwt and routing

--- README.md
+++ README.md
@@ -26,3 +26,25 @@ go get -u gorm.io/driver/mysql
 go get -u github.com/cosmtrek/air
 ```
 
+# Package
+
+addition package use in this project
+
+## JWT (JSON Web Tokens)
+
+JSON Web Tokens are an open, industry standard [RFC 7519](https://tools.ietf.org/html/rfc7519) method for representing claims securely between two parties. [JSON Web Tokens - jwt.io](https://jwt.io/)
+
+```bash
+go get -u github.com/golang-jwt/jwt
+```
+
+## Middleware (Echo)
+
+```sh
+ go get github.com/labstack/echo/middleware
+ 
+ "github.com/labstack/echo/v4/middleware"
+```
+
+
+
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -16,6 +19,13 @@ type User struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
+}
+
+type jwtCustomClaims struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	jwt.StandardClaims
 }
 
 var Db *gorm.DB
@@ -36,16 +46,32 @@ func main() {
 	// Start Server
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	// Routing
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	// Authication Router
+	e.POST("/login", login)
+
+	// Unauthenticated route
+	e.GET("/public", accessible)
 
 	// User Router
 	e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser)
 	e.PUT("/users/:id", updateUser)
 	e.DELETE("/users/:id", deleteUser)
+
+	// Restricted group
+	r := e.Group("/private")
+	// Configure middleware with the custom claims type
+	config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte("secret"),
+	}
+	r.Use(middleware.JWTWithConfig(config))
+	r.GET("", restricted)
 
 	// Run Server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -107,4 +133,69 @@ func deleteUser(c echo.Context) error {
 	}
 
 	return c.String(http.StatusOK, fmt.Sprintf("User Id %v was deleted", id))
+}
+
+// Login
+func login(c echo.Context) error {
+	// Content-Type: multipart/form-data
+	name := c.FormValue("name")
+	password := c.FormValue("password")
+
+	user, err := getUserByName(name)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return c.String(http.StatusNotFound, "Not found")
+		}
+
+		return c.String(http.StatusBadRequest, "Something bad happend")
+	}
+
+	// Throws unauthorized error
+	if name != user.Name || password != user.Password {
+		return echo.ErrUnauthorized
+	}
+
+	// Set custom claims
+	claims := &jwtCustomClaims{
+		user.ID,
+		user.Name,
+		user.Email,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
+		},
+	}
+
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
+
+// Get user by name
+func getUserByName(name string) (User, error) {
+	var user User
+	if err := Db.First(&user, "name = ?", name).Error; err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Accessible")
+}
+
+func restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	name := claims.Name
+	return c.String(http.StatusOK, "Welcome "+name+"!")
 }
